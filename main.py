d7b5884d12bf112e809db1ae0b7ecfbf9bccce62
Yassine El Haddad <yel-hadd@mail.com>
2023-04-14 23:18:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+from os import system
 import pandas as pd
 import docx
-from replace_word import replace_word_in_docx as rp
+
 
 def generate_docs(excel_path, word_path):
+    system("rm -rf output/*")
     df = pd.read_excel(excel_path)
-    
+
     columns = list(df.columns)
 
     for index, row in df.iterrows():
         doc = docx.Document(word_path)
         for column in columns:
             pattern = f"<<{column}>>"
-            outpath = f"./output/{row[columns[0]]} - {row[columns[1]]}.docx"
+            try:
+                outpath = f"./output/{row[columns[0]]}_{row[columns[1]].replace(' ', '_').lower()}.docx"
+            except:
+                outpath = f"{index}.docx"
             for paragraph in doc.paragraphs:
                 if pattern in paragraph.text:
                     paragraph.text = paragraph.text.replace(pattern, str(row[column]))
             for table in doc.tables:
-                for row in table.rows:
-                    for cell in row.cells:
+                for r in table.rows:
+                    for cell in r.cells:
                         if pattern in cell.text:
                             cell.text = cell.text.replace(pattern, str(row[column]))
         doc.save(outpath)
         print(outpath + " has been written")
 
-generate_docs("./excel/path/here", "./word/path/here")
 
+generate_docs("./excel/path/here", "./word/path/here")
